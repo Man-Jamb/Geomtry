eaa627479db5293f5723c51c9a38e02875e2e6fe
Man-Jamb <78638691+Man-Jamb@users.noreply.github.com>
2021-03-02 16:45:26 +0300

    Add

---
+++ Geometry/Geometry/GeometryLib.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Geometry
+{
+    interface Geometry
+    {
+        double Square();
+    }
+
+
+    public class Triangle : Geometry
+    {
+        double a, b, c;
+
+        public Triangle(double A, double B, double C)
+        {
+            if ((A <= 0) || (B <= 0) || (C <= 0))
+            {
+                throw new ArgumentException();
+            }
+            if ((A >= B + C) || (B >= A + C) || (C >= A + B))
+            {
+                throw new ArgumentException();
+            }
+
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public double Square()
+        {
+            double p = (a + b + c) / 2;
+            return (Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+        }
+
+        public bool IsRight()
+        {
+            if (a * a == b * b + c * c)
+            {
+                return true;
+            }
+            if (b * b == a * a + c * c)
+            {
+                return true;
+            }
+            if (c * c == a * a + b * b)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class Circle : Geometry
+    {
+        double r;
+
+        public Circle(double R)
+        {
+            if (R <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            r = R;
+        }
+
+        public double Square()
+        {
+            return (Math.PI * r * r);
+        }
+    }
+}
---
+++ Geometry/GeometryTests/TriangleTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void Square_3and4and5_6Returned()
+        {
+            //arrange
+            Triangle triangle = new Triangle(3, 4, 5);
+            const double expected = 6;
+
+            //act
+            double actual = triangle.Square();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsRight_3and4and5_TrueReturned()
+        {
+            //arrange
+            Triangle triangle = new Triangle(3, 4, 5);
+            const bool expected = true;
+
+            //act
+            bool actual = triangle.IsRight();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass()]
+    public class CircleTests
+    {
+        [TestMethod()]
+        public void Square_5_78Returned()
+        {
+            //arrange
+            Circle circle = new Circle(5);
+            const double expected = 78.54;
+            const double delta = 0.005;
+
+            //act
+            double actual = circle.Square();
+
+            //assert
+            Assert.AreEqual(expected, actual, delta);
+        }
+    }
+}
+
+
